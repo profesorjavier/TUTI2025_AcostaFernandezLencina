1fcd6be65b1adbe05509926f4810da35e25e703b
Francisco Javier Acosta <profesorjaviertecnologia@gmail.com>
2025-05-21 22:19:30 -0300

    Creación del proyecto

---
+++ src/main/java/com/tuti2025/acostafernandezlencina/Tuti2025AcostaFernandezLencinaApplication.java
@@ -0,0 +1,13 @@
+package com.tuti2025.acostafernandezlencina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuti2025AcostaFernandezLencinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuti2025AcostaFernandezLencinaApplication.class, args);
+	}
+
+}
